9a6548612839c0d821976f5afb7f7b6dd0f62873
Ernesto Ulate Ramirez <netou18@gmail.com>
2018-10-03 16:02:56 -0600

    initial commit

---
+++ src/obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o.d
@@ -0,0 +1,5 @@
+/home/ochesto/TEC/Arqui-II/Taller_3/src/obj/local/armeabi-v7a/objs/cpufeatures/cpu-features.o: \
+  /opt/android-ndk-r18/sources/android/cpufeatures/cpu-features.c \
+  /opt/android-ndk-r18/sources/android/cpufeatures/cpu-features.h
+
+/opt/android-ndk-r18/sources/android/cpufeatures/cpu-features.h:
---
+++ src/obj/local/armeabi-v7a/objs/pi_par/pi_par.o.d
@@ -0,0 +1,5 @@
+/home/jef/Dropbox/TEC/2017/Arquitectura_de_Computadores_II/Talleres/Taller_4_OpenMP_Android/src/obj/local/armeabi-v7a/objs/pi_par/pi_par.o: \
+  /home/jef/Dropbox/TEC/2017/Arquitectura_de_Computadores_II/Talleres/Taller_4_OpenMP_Android/src/jni/pi_par.c \
+  /home/jef/Dropbox/TEC/2017/Arquitectura_de_Computadores_II/Talleres/Taller_4_OpenMP_Android/src/jni/omp.h
+
+/home/jef/Dropbox/TEC/2017/Arquitectura_de_Computadores_II/Talleres/Taller_4_OpenMP_Android/src/jni/omp.h:
